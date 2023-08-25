650c6611c01c08f5b5d9c4828fd37eded87f7b85
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-25 14:38:25 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -14,6 +14,44 @@ bot.on("text", async msg => {
     return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
 });
 
+bot.on('/inlineKeyboard', msg => {
+
+    let replyMarkup = bot.inlineKeyboard([
+        [
+            bot.inlineButton('callback', {callback: 'this_is_data'}),
+            bot.inlineButton('inline', {inline: 'some query'})
+        ], [
+            bot.inlineButton('url', {url: 'https://telegram.org'})
+        ]
+    ]);
+
+    return bot.sendMessage(msg.from.id, 'Inline keyboard example.', {replyMarkup});
+
+});
+
+// Inline button callback
+bot.on('callbackQuery', msg => {
+    // User message alert
+    return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
+});
+
+// Inline query
+bot.on('inlineQuery', msg => {
+
+    const query = msg.query;
+    const answers = bot.answerList(msg.id);
+
+    answers.addArticle({
+        id: 'query',
+        title: 'Inline Query',
+        description: `Your query: ${ query }`,
+        message_text: 'Click!'
+    });
+
+    return bot.answerQuery(answers);
+
+});
+
 export default bot
 
 //YsbcVL8dXcW0lAY7
