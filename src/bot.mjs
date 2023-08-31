4c403616afa11ce68db7ddeeff44f891988b83fa
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-08-31 20:22:33 +0200

    Add files via upload

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,58 +1,154 @@
 import TeleBot from "telebot"
 // const openai = require('openai');
 // const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
+import { nanoid } from 'nanoid'
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot( {token: process.env.TELEGRAM_BOT_TOKEN,usePlugins: ['askUser']})
 
-
-bot.on("text", async msg => {
+bot.on('/start', msg => {
     let replyMarkup = bot.keyboard([
-        ['/buttons', '/inlineKeyboard'],
-        ['/start', '/hide']
+        ['Створити клас'],
+        ['Приєднатися в клас, як учень', 'Приєднатися в клас, як вчитель']
     ], {resize: true});
 
-    return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
-});
 
-bot.on('/inlineKeyboard', msg => {
-
-    let replyMarkup = bot.inlineKeyboard([
-        [
-            bot.inlineButton('callback', {callback: 'this_is_data'}),
-            bot.inlineButton('inline', {inline: 'some query'})
-        ], [
-            bot.inlineButton('url', {url: 'https://telegram.org'})
-        ]
-    ]);
-
-    return bot.sendMessage(msg.from.id, 'Inline keyboard example.', {replyMarkup});
-
-});
-
-// Inline button callback
-bot.on('callbackQuery', msg => {
-    // User message alert
-    console.log(msg)
-    bot.sendMessage(msg.from.id,msg.data)
-    return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
-});
-
-// Inline query
-bot.on('inlineQuery', msg => {
-
-    const query = msg.query;
-    const answers = bot.answerList(msg.id);
-
-    answers.addArticle({
-        id: 'query',
-        title: 'Inline Query',
-        description: `Your query: ${ query }`,
-        message_text: 'Click!'
-    });
-
-    return bot.answerQuery(answers);
+    return bot.sendMessage(msg.from.id, `🤖 Привіт, ${msg.from.name}! Я ваш особистий навчальний асистент! З моєю допомогою ви зможете легко керувати навчальним процесом. Ось деякі з функцій, які я можу виконувати:
+    
+    🏫 Створення та управління навчальними групами
+    📚 Посилання на корисні матеріали від викладачів
+    📌 Важливі контакти та оголошення
+    📋 Завдання від викладачів та їх здача
+    📁 Передавання файлів у групі
+    📝 Створення та виконання тестів
+    🗓️ Планування подій та зустрічей
+    📊 Оцінки та відвідуваність уроків
+    📚 Матеріали для навчання та підсумки уроків
+    
+    ...та багато іншого! Просто введіть команду або натисніть на кнопку, щоб розпочати. Я готовий допомогти вам у всьому, пов'язаному з навчанням. Почнімо разом! 🎓`, {ask: 'class',replyMarkup});
 
 });
+
+bot.on('ask.class', async msg => {
+
+if(msg.text === "Створити клас"){
+    let idClass = [nanoid(),nanoid()]
+    const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('artem-school').collection('classrooms');
+    const result = await coll.insertOne({idT:idClass[1],idS:idClass[0],files:[],events:[],homework:[],marks:[],lessons:[],statisticks:[],users:[{name:msg.from.name, username:msg.from.username, id:msg.from.id, role:"Вчитель"}]})
+    await client.close();
+    return await bot.sendMessage(msg.from.id, `Клас успішно створився!
+    <code>${idClass[0]}</code> - id для приєднання учня в клас
+    <code>${idClass[1]}</code> - id для приєднання вчителя в клас
+    `, {ask: 'actiont', parseMode: 'html'});
+}else if(msg.text === "Приєднатися в клас, як учень"){
+    const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('artem-school').collection('classrooms');
+            const filter = {idS: msg.text};
+            const cursor = coll.find(filter);
+            const result = await cursor.toArray();
+            if(result[0]){
+                const users = {"users": [{name:msg.from.name, username:msg.from.username, id:msg.from.id, role:"Вчитель"},...result[0].users]}
+                coll.updateOne(
+                    {_id: new ObjectId(id._id)},
+                    {
+                      $set: { ...users},
+                      $currentDate: { lastModified: true }
+                    }
+                 );
+                 await client.close();
+                 return await bot.sendMessage(msg.from.id, `Ви успішно доєдналися до класу`, {ask: 'actiont'});
+            }else{
+                await client.close();
+                return await bot.sendMessage(msg.from.id, `Ви вели неправильний id класу`, {ask: 'class'});
+            }  
+}else if(msg.text === "Приєднатися в клас, як вчитель"){
+    const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.NEXT_PUBLIC_DATABASE_USER}:${process.env.NEXT_PUBLIC_DATABASE_PASSWORD}@${process.env.NEXT_PUBLIC_DATABASE}/?retryWrites=true&w=majority`,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('artem-school').collection('classrooms');
+            const filter = {idS: msg.text};
+            const cursor = coll.find(filter);
+            const result = await cursor.toArray();
+            if(result[0]){
+                const users = {"users": [{name:msg.from.name, username:msg.from.username, id:msg.from.id, role:"Учень"},...result[0].users]}
+                coll.updateOne(
+                    {_id: new ObjectId(id._id)},
+                    {
+                      $set: { ...users},
+                      $currentDate: { lastModified: true }
+                    }
+                 );
+                 await client.close();
+                 return await bot.sendMessage(msg.from.id, `Ви успішно доєдналися до класу`, {ask: 'actions'});
+            }else{
+                await client.close();
+                return await bot.sendMessage(msg.from.id, `Ви вели неправильний id класу`, {ask: 'class'});
+            }  
+}else{
+    let replyMarkup = bot.keyboard([
+        ['Створити клас'],
+        ['Приєднатися в клас, як учень', 'Приєднатися в клас, як вчитель']
+    ], {resize: true});
+    return bot.sendMessage(msg.from.id, `Error`, {ask: 'class',replyMarkup});
+}
+});
+
+// bot.on("text", async msg => {
+//     let replyMarkup = bot.keyboard([
+//         ['/buttons', '/inlineKeyboard'],
+//         ['/start', '/hide']
+//     ], {resize: true});
+
+//     return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
+// });
+
+// bot.on('/inlineKeyboard', msg => {
+
+//     let replyMarkup = bot.inlineKeyboard([
+//         [
+//             bot.inlineButton('callback', {callback: 'this_is_data'}),
+//             bot.inlineButton('inline', {inline: 'some query'})
+//         ], [
+//             bot.inlineButton('url', {url: 'https://telegram.org'})
+//         ]
+//     ]);
+
+//     return bot.sendMessage(msg.from.id, 'Inline keyboard example.', {replyMarkup});
+
+// });
+
+// // Inline button callback
+// bot.on('callbackQuery', msg => {
+//     // User message alert
+//     console.log(msg)
+//     bot.sendMessage(msg.from.id,msg.data)
+//     return bot.answerCallbackQuery(msg.id, `Inline button callback: ${ msg.data }`, true);
+// });
+
+// // Inline query
+// bot.on('inlineQuery', msg => {
+
+//     const query = msg.query;
+//     const answers = bot.answerList(msg.id);
+
+//     answers.addArticle({
+//         id: 'query',
+//         title: 'Inline Query',
+//         description: `Your query: ${ query }`,
+//         message_text: 'Click!'
+//     });
+
+//     return bot.answerQuery(answers);
+
+// });
 
 export default bot
 
