2ef8bfd01e487c34007a7126b1b3533afb76de49
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-01 15:52:58 +0200

    Update and rename telgram.mjs to telegram.mjs

--- api/telgram.mjs
+++
@@ -1,6 +0,0 @@
-import {start} from "telebot-vercel"
-import bot from "../src/bot.mjs"
-export default async function handler(req, res) {
-        res.status(200)
-}
-// export default start({ bot })
