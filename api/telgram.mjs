26d66cfb89d10f93b9705db6d882bb4785b4ec53
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-01 15:51:11 +0200

    Update telgram.mjs

--- api/telgram.mjs
+++ api/telgram.mjs
@@ -1,4 +1,6 @@
 import {start} from "telebot-vercel"
 import bot from "../src/bot.mjs"
-
+export default async function handler(req, res) {
+        res.status(200)
+}
 export default start({ bot })
