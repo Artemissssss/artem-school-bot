3f3997c39510fcaebb29cde601868a4838a902c0
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-11 20:12:04 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -242,6 +242,7 @@ bot.on('*', async msg => {
     }
 
     if(userStatus[msg.from.id] === undefined && (text !== "/start" || text.indexOf("/start") !== 0)){
+        console.log(text.indexOf("/start") !== 0, text)
         const client = await MongoClient.connect(
             `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URI}/?retryWrites=true&w=majority`,
             { useNewUrlParser: true, useUnifiedTopology: true }
