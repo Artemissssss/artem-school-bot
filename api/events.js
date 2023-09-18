aeffcdd09ab9df5546b61f3a7377093322bcd4bb
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-18 18:40:53 +0200

    Update events.js

--- api/events.js
+++ api/events.js
@@ -43,7 +43,9 @@ export default async function handler(req, res) {
             let eventList = newEvent[0].events.filter(arr => arr.date === currentDateInUkraine);
             eventArr = 0+eventList.length;
         };
-        await bot.sendMessage(newArr[i], `У вас сьогодні ${eventArr} подій`)
+        if(eventArr){
+            await bot.sendMessage(newArr[i], `У вас сьогодні ${eventArr} подій`)
+        }
     }
 
     await res.status(200).json({hello:true})
