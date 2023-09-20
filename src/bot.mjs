4a9f4c22d36d239ef57072bba39722bde884efcd
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-20 14:07:14 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -147,18 +147,18 @@ bot.on('*', async msg => {
             return null;
         }else if(msg.reply_to_message?.forward_from.id){
             if(text){
-                return bot.sendMessage(msg.reply_to_message.forward_from.id, text);
-            }else if(msg.photo){
-                if(msg.photo.caption){
-                    return bot.sendPhoto(msg.reply_to_message.forward_from.id, msg.photo[0].file_id, {caption:msg.photo.caption})
+                returnbot.sendMessage(userChat[msg.from.id], text);
+            }else if(msg?.photo[0].file_id){
+                if(msg.photo?.caption){
+                    return bot.sendPhoto(userChat[msg.from.id], msg.photo[0].file_id, {caption:msg.photo.caption})
                 }else{
-                    return bot.sendPhoto(msg.reply_to_message.forward_from.id, msg.photo[0].file_id)
+                    return bot.sendPhoto(userChat[msg.from.id], msg.photo[0].file_id)
                 }
-            }else if(msg.document){
+            }else if(msg?.document.thumbnail.file_id){
                 if(msg.document.caption){
-                    return bot.sendDocument(msg.reply_to_message.forward_from.id, msg.document.thumbnail.file_id,{caption:msg.document.caption})
+                    return bot.sendDocument(userChat[msg.from.id], msg.document.thumbnail.file_id,{caption:msg.document.caption})
                 }else{
-                    return bot.sendDocument(msg.reply_to_message.forward_from.id, msg.document.thumbnail.file_id)
+                    return bot.sendDocument(userChat[msg.from.id], msg.document.thumbnail.file_id)
                 }
             }
         }
