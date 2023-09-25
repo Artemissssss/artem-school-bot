4e2d07d03aa206a431848c00364aef670bd35be4
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-25 19:49:13 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -6,7 +6,50 @@ const { Markup } = pkg;
 import { MongoClient,ObjectId } from 'mongodb';
 import { nanoid } from 'nanoid'
 import moment from 'moment-timezone';
-import zustrich from "./zustrich";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+import { getDatabase, set, ref ,push, child, onValue, remove} from
+"https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
+
+
+async function zustrich (type, time,idRoom) {
+    const firebaseConfig = {
+    apiKey: process.env.API_KEY_FIREBASE,
+    authDomain: "zustrich-be18b.firebaseapp.com",
+    databaseURL: "https://zustrich-be18b-default-rtdb.firebaseio.com",
+    projectId: "zustrich-be18b",
+    storageBucket: "zustrich-be18b.appspot.com",
+    messagingSenderId: "125274425353",
+    appId: "1:125274425353:web:6f38bd6892f88805ee10d8",
+    measurementId: "G-4MH57WVHBD"
+    };
+
+    if(type){
+        const app = initializeApp(firebaseConfig);
+        const database = getDatabase(app);
+        const room = nanoid()
+        await set(ref(database, "rooms/" + room), {
+            time: time,
+            members:[]
+        });
+
+        return {idRoom: room}
+    }else{
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    const starCountRef = ref(database, 'rooms/');
+
+    onValue(starCountRef, (snapshot) => {
+        snapshot.forEach(childSnapshot => {
+            if (childSnapshot.key === idRoom) {
+                let newUs = childSnapshot.val().members;
+                return {members: newUs}
+            }
+        });
+        return {members: []}
+    });
+    }
+
+}
 
 const bot = new TeleBot( {token: process.env.TELEGRAM_BOT_TOKEN})
 let lastUserMessage = {};
