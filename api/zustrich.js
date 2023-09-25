c8543e8ee71ea44736efde93c5087caed7f60388
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-09-25 19:20:12 +0200

    Update zustrich.js

--- api/zustrich.js
+++ api/zustrich.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app.js';
+import { initializeApp } from 'firebase/app/dist/index.cjs.js';
 import { getDatabase, set, ref, onValue,  } from 'firebase/database';
 import { nanoid } from 'nanoid'
 
