a6ec516c460de668207b4e198d9ca8437c161c19
Artem Vainshtein <64923345+Artemissssss@users.noreply.github.com>
2023-10-14 23:18:09 +0200

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -771,7 +771,7 @@ if(text === "Написати учаснику"){
             }else{
                 console.log(result1[0].events)
                 for(let i = 0; i<result1[0].events.length;i++){
-                    if(!result1[0]?.status){
+                    if(!result1[0].events[i]?.status){
                         await bot.sendMessage(msg.chat.id, `${result1[0].events[i].text}\n\n\nО ${result1[0].events[i].date} ${result1[0].events[i].time}\n\nДля: ${result1[0].events[i].who}`);
                         if(msg.chat.type ==="private" && userStatus[msg.from.id]){
                             await bot.sendMessage(msg.from.id, `${result1[0].events[i].id}`);
